d9d99e6c82bbe769ad4374f37a1be6aabc378908
Andrés Atencio <aatencio@manas.tech>
2019-08-25 02:30:12 -0300

    Add Sequelize

--- api/server.js
+++ api/server.js
@@ -8,6 +8,23 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('mysql', 'root', 'example', {
+  host: 'db',
+  dialect: 'mariadb'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
