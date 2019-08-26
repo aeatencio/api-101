2382117bb8699dc02a80293a5c915ab59bff952a
Andrés Atencio <aatencio@manas.tech>
2019-08-26 10:58:53 -0300

    Get examples

--- api/routes/examples.js
+++ api/routes/examples.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var models = require("../models");
 
-/* GET examples listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.example.findAll({
+    attributes: ['name']
+  }).then(function(examples) {
+    res.send(examples);
+  });
 });
 
 module.exports = router;
